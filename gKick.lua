dd6c9e6ad0d2433c4a5152b2052c6fa90144b349
lownignitus <lownignitus@gmail.com>
2016-10-11 20:28:20 -0700

    Simple addon to boot guild members
    allows use of /gk <name> or /gkick <name>

---
+++ gKick.lua
@@ -0,0 +1,33 @@
+local CF = CreateFrame
+local addon_name = "gKick"
+
+SLASH_GKICK1 = "/gKick" or "/gkick"
+SLASH_GKICK_GK1 = "/gk"
+
+local gkEvents_table = {}
+
+gkEvents_table.eventFrame = CF("Frame")
+gkEvents_table.eventFrame:RegisterEvent("ADDON_LOADED")
+
+gkEvents_table.eventFrame:SetScript("OnEvent", function(self, event, ...)
+	gkEvents_table.eventFrame[event](self, ...)
+end)
+
+function gkEvents_table.eventFrame:ADDON_LOADED(AddOn)
+	if AddOn ~= addon_name then
+		return
+	end
+
+	gkEvents_table.eventFrame:UnregisterEvent("ADDON_LOADED")
+
+	ChatFrame1:AddMessage(GetAddOnMetadata(addon_name, "Title") .. " " .. GetAddOnMetadata(addon_name, "Version"))
+	ChatFrame1:AddMessage("|cff71C671To remove guild member type /gKick or /gk|r")
+end
+
+function SlashCmdList.GKICK(msg)
+	GuildUninvite(msg)
+end
+
+function SlashCmdList.GKICK_GK(msg)
+	GuildUninvite(msg)
+end
